2eb58e3062a8a07a290f84281c5d06a31e73a6d7
Andreas Mathis <andreas.mathis@cern.ch>
2017-04-28 10:32:58 +0200

    Add dummy pedestal

--- Detectors/TPC/simulation/include/TPCSimulation/SAMPAProcessing.h
+++ Detectors/TPC/simulation/include/TPCSimulation/SAMPAProcessing.h
@@ -92,6 +92,7 @@ float SAMPAProcessing::makeSignal(float ADCcounts, const PadSecPos& padSecPos)
   float signal = ADCcounts;
   /// \todo Pedestal to be implemented in baseline class
 //  signal += baseline.getPedestal(padSecPos);
+  signal += 70.f;
   signal += baseline.getNoise(padSecPos);
   return sampa.getADCSaturation(signal);
 }
